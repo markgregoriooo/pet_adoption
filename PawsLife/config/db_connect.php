84eafc9bad14730ba6011bd8d2da8b88da61b46b
markgregoriooo <magregorio@my.cspc.edu.ph>
2024-12-01 10:38:50 +0800

    Delete PawsLife directory

--- PawsLife/config/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    // connect to database
-    $conn = mysqli_connect('localhost', 'IT2A', 'petadoption', 'pawslife_db');
-
-    //check connection
-    if(!$conn){ 
-        die('Connection failed: ' . mysqli_connect_error());
-    }
-?>
